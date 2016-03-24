e32db1c8a1d9390d3346e2c56269f026c4f1ab32
Karlis Andersons <karlis.andersons@aurumit.com>
2016-03-24 15:03:59 +0000

    Add page sonata block

---
+++ Sonata/Block/PageBlock.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ait\CmsBundle\Sonata\Block;
+
+use Doctrine\ORM\EntityManager;
+use Sonata\BlockBundle\Block\BaseBlockService;
+use Sonata\BlockBundle\Block\BlockContextInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+
+class PageBlock extends BaseBlockService
+{
+    protected $enm;
+
+    protected $pageClass;
+
+    public function __construct($name, EngineInterface $templating, EntityManager $enm, $pageClass)
+    {
+        parent::__construct($name, $templating);
+        $this->enm = $enm;
+        $this->pageClass = $pageClass;
+    }
+
+
+    public function execute(BlockContextInterface $blockContext, Response $response = null)
+    {
+        $pages = $this->enm->createQueryBuilder()
+            ->select('p.id, p.name')
+            ->from('AppBundle:Page', 'p')
+            ->orderBy('p.id', 'DESC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getArrayResult();
+
+        return $this->renderResponse('AitCmsBundle:Sonata/Block:page_block.html.twig', [
+            'block' => $blockContext->getBlock(),
+            'pages' => $pages,
+        ], $response);
+    }
+}
